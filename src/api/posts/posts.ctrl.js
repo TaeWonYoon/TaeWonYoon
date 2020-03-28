0ac7b057055f97b5621038d6a3f6824af3434e0d
Your Name <you@example.com>
2020-03-28 18:26:44 +0900

    20-03-28

---
+++ src/api/posts/posts.ctrl.js
@@ -0,0 +1,148 @@
+import Post from '../../models/post';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const getPostById = async (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+  try {
+    const post = await Post.findById(id);
+    // 포스트가 존재하지 않을 때
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const checkOwnPost = (ctx, next) => {
+const { user, post } = ctx.state;
+if (post.user._id.toString() !== user._id) {
+  ctx.status = 403;
+  return;
+}
+return next();
+};
+
+export const write = async ctx => {
+  const schema = Joi.object().keys({
+    // 객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), // required() 가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array()
+      .items(Joi.string())
+      .required(), // 문자열로 이루어진 배열
+  });
+
+  // 검증 후, 검증 실패시 에러처리
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+    user: ctx.state.user,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const list = async ctx => {
+
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if(page < 1) {
+    ctx.status = 400;
+    return;
+  }
+  const { tag, username } = ctx.query;
+  // tag, username 값이 유효하면 객체 안에 넣고 그렇지 않으면 넣지 않음
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
+  try{
+  const posts = await Post.find(query)
+  .sort({ _id: -1})  //내림차순 _id를 1로하면 오름차순
+  .limit(20)  //제한하기
+  .skip((page - 1) * 10)
+  .exec();
+const postCount = await Post.countDocuments(query).exec(0);
+ctx.set('Last-Pages', Math.ceil(postCount / 10));
+  ctx.body = posts
+  .map(post => post.toJSON())
+  .map(post => ({
+    ...post,
+    body:
+    post.body.length < 200 ? post.body : `${post.body.slice(0, 100)}...`,
+  }))
+} catch(e) {
+  ctx.throw(500, e);
+ }
+};
+
+export const read = ctx => {
+  ctx.body = ctx.state.post;
+};
+
+export const remove = async ctx => {
+  const { id } = ctx.params;
+  try{
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204;
+  } catch(e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const update = async ctx => {
+  const { id } = ctx.params;
+  // write 에서 사용한 schema 와 비슷한데, required() 가 없습니다.
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  // 검증 후, 검증 실패시 에러처리
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400; // Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+    if(!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch(e) {
+    ctx.throw(500, e);
+  }
+};
